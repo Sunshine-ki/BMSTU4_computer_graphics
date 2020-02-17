bb941f3e6e70c6be0e33df4d6b236dbd5e07e95c
Sukocheva Alice <sukocheva.alis@mail.ru>
2020-02-17 10:27:22 +0300

    Добавлен тестовый пример с решением

---
+++ lab_01/solution.py
@@ -0,0 +1,52 @@
+def find_coefficients(a, b):
+	# Проверка на прямую,парал. оси у
+	k = (a[1] - b[1]) / (a[0] - b[0])
+	b = a[1] - (a[0] * k) 
+	# print(a[0], "- ( ",a[1], "*", k, ")" )
+	return k, b
+
+def perpendicular_lines(k1):
+	# Перпендикулярность прямых к1*к2=-1.
+	return -1 / k1
+	
+def find_coefficients_b(a, k):
+	return a[1] - (a[0] * k)
+
+# Точка пересечения прямых a и b.
+def intersection_point(a, b):
+	print(a, b)
+	print("-1 * (", a[1], "-", b[1],") / (", a[0], "-", b[0], ")")
+	x = -1 * (a[1] - b[1]) / (a[0] - b[0])
+	print(a[0], "*", x, "+", a[1])
+	y = a[0] * x + a[1]
+	return x, y
+
+
+
+def main():
+	point_lst = [(5.0, -1.0), (4.0, -8.0), (-4.0, -4.0)]
+	# print(point_lst)
+
+	# Ищем АС.
+	line_one = find_coefficients(point_lst[0], point_lst[2])
+	# print(line_one)
+
+	# Ищем BQ. (Высота)
+	k = perpendicular_lines(line_one[0])
+	line_perpendicular_one = (k, find_coefficients_b(point_lst[1], k))
+	# print("BQ = ", line_perpendicular_one)
+
+	# Ищем BC.
+	line_two = find_coefficients(point_lst[1], point_lst[2])
+	# print(line_two)
+
+	# Ищем AP. (Вторая высота).
+	k = perpendicular_lines(line_two[0])
+	line_perpendicular_two = (k, find_coefficients_b(point_lst[0],k))
+	# print("AP = ", line_perpendicular_two)
+
+	# Ищем точку пересечения высот.
+	print(intersection_point(line_perpendicular_one, line_perpendicular_two))
+
+if __name__ == '__main__':
+	main()
