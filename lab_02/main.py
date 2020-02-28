3ff7d4373b54e69a7ca2768b3971aba1d19fc2a1
Sukocheva Alice <sukocheva.alis@mail.ru>
2020-02-28 19:47:15 +0300

    Добавлено обновление рисунка

--- lab_02/main.py
+++ lab_02/main.py
@@ -7,8 +7,8 @@ from math import *
 from tkinter import *
 from tkinter import messagebox as mb
 
-a = 80
-b = 120
+a = 40
+b = 60
 win_size = 800
 radius_point = 3
 list_point = []
@@ -78,6 +78,10 @@ def inverse_func(matrix):
 
     # Ищем определитель матрицы
     det = determinant(matrix_res)
+    if det == 0:
+        print("det = ", det)
+        # det = 1
+        return
 
     # Ищем алгебраич. доп.
     for i in range(3):
@@ -167,8 +171,26 @@ def rotation():
     if (angle == false):
         return
 
-    dx = -list_point[len(list_point) - 1][0]
-    dy = -list_point[len(list_point) - 1][1]
+    xm = entry_angle_xm.get()
+    if (check_answer(xm) == False):
+        return
+    xm = float_answer(xm)
+    if (xm == false):
+        return
+
+    ym = entry_angle_ym.get()
+    if (check_answer(ym) == False):
+        return
+    ym = float_answer(ym)
+    if (ym == false):
+        return
+
+    # print("angle = ", angle, "Xm = ", xm, "Ym = ", ym)
+
+    dx = -xm
+    dy = -ym
+    # dx = -list_point[len(list_point) - 1][0]
+    # dy = -list_point[len(list_point) - 1][1]
 
     # Переводим в радианы.
     angle *= pi / 180
@@ -229,6 +251,10 @@ def scale():
     if (ky == false):
         return
 
+    if kx == 0 or ky == 0:
+        print_error("При таких значениях рисунка не будет!")
+        return
+
     xm = entry_xm.get()
     if (check_answer(xm) == False):
         return
@@ -243,7 +269,7 @@ def scale():
     if (ym == false):
         return
 
-    print("kx = ", kx, "ky = ", ky, "xm = ", xm, "ym = ", ym)
+    # print("kx = ", kx, "ky = ", ky, "xm = ", xm, "ym = ", ym)
 
     matrix = [[kx, 0, 0],
               [0, ky, 0],
@@ -253,13 +279,9 @@ def scale():
                   [0, 1, 0],
                   [xm, ym, 1]]
 
-    matrix_mov_inv = [[1, 0, 0],
-                      [0, 1, 0],
-                      [-xm, -ym, 1]]
-
     matrix_res = multiplication_matrix(matrix_mov, matrix)
     matrix_mov[2][0], matrix_mov[2][1] = -xm, -ym
-    matrix_res = multiplication_matrix(matrix_res, matrix_mov_inv)
+    matrix_res = multiplication_matrix(matrix_res, matrix_mov)
 
     global inverse_matrix
     # inverse_matrix = inverse_func(matrix)
@@ -287,10 +309,25 @@ def print_scene():
     canv.create_line(win_size/2, win_size, win_size/2, 0, width=2, arrow=LAST)
     canv.create_line(0, win_size/2, win_size, win_size/2, width=2, arrow=LAST)
 
-    for i in range(len(list_point) - 2):
+    for i in range(len(list_point) - 2 - 8):
         paint_point(list_point[i])
         paint_line(list_point[i], list_point[i + 1])
-    paint_point(list_point[len(list_point) - 1])
+    paint_point(list_point[len(list_point) - 1 - 8])
+
+    paint_line(list_point[len(list_point) - 8],
+               list_point[len(list_point) - 7])
+    paint_line(list_point[len(list_point) - 6],
+               list_point[len(list_point) - 5])
+
+    paint_line(list_point[len(list_point) - 4],
+               list_point[len(list_point) - 3])
+    paint_line(list_point[len(list_point) - 2],
+               list_point[len(list_point) - 1])
+    # paint_line([-250, 150], [250, 150])
+    # paint_line([-250, -150], [250, -150])
+
+    # paint_line([-250, -150], [-250, 150])
+    # paint_line([250, -150], [250, 150])
 
 
 def create_scene():
@@ -303,10 +340,49 @@ def create_scene():
             pass
     list_point.append([0, 0, 1])
 
+    list_point.append([-250, 150, 1])
+    list_point.append([250, 150, 1])
+
+    list_point.append([-250, -150, 1])
+    list_point.append([250, -150, 1])
+
+    list_point.append([-250, -150, 1])
+    list_point.append([-250, 150, 1])
+
+    list_point.append([250, -150, 1])
+    list_point.append([250, 150, 1])
+
     print_scene()
 
 
+def return_all():
+    global inverse_matrix
+    inverse_matrix = [[1, 0, 0],
+                      [0, 1, 0],
+                      [0, 0, 1]]
+    for i in range(len(list_point) - 1, -1, -1):
+        del list_point[i]
+    # print("Массив: ", list_point)
+    create_scene()
+
+
 if __name__ == "__main__":
+
+    # root_a_b = Tk()
+    # root_a_b.title('Лабораторная работа №2')
+    # root_a_b.geometry("600x400")
+    # root_a_b.configure(bg="lavender")
+    # root_a_b.resizable(width=False, height=False)
+
+    # label = Label(root, text="kx:", bg="lavender").place(x=800,
+    #                                                      y=450, anchor="w", width=100)
+
+    # entry_dx = Entry(root_a_b, width="50")
+    # entry_dx.place(x=100, y=100, anchor="center", width=150)
+
+    # root_a_b.mainloop()
+
+    #
     root = Tk()
     root.title('Лабораторная работа №2')
     root.geometry("1200x800")
@@ -344,33 +420,47 @@ if __name__ == "__main__":
     button.place(x=1000, y=200, anchor="center")
     label = Label(root, text="Угол:", bg="lavender").place(x=800,
                                                            y=250, anchor="w", width=100)
+    label = Label(root, text="Xm:", bg="lavender").place(x=800,
+                                                         y=300, anchor="w", width=100)
+    label = Label(root, text="Ym:", bg="lavender").place(x=800,
+                                                         y=350, anchor="w", width=100)
     entry_angle = Entry(root, width="50")
     entry_angle.place(x=1000, y=250, anchor="center", width=150)
 
+    entry_angle_xm = Entry(root, width="50")
+    entry_angle_xm.place(x=1000, y=300, anchor="center", width=150)
+
+    entry_angle_ym = Entry(root, width="50")
+    entry_angle_ym.place(x=1000, y=350, anchor="center", width=150)
+
     button = Button(text="Масштабировать", width=15,
                     command=scale,  bg="thistle3")
-    button.place(x=1000, y=300, anchor="center")
+    button.place(x=1000, y=400, anchor="center")
 
     entry_kx = Entry(root, width="50")
-    entry_kx.place(x=1000, y=350, anchor="center", width=150)
+    entry_kx.place(x=1000, y=450, anchor="center", width=150)
 
     entry_ky = Entry(root, width="50")
-    entry_ky.place(x=1000, y=400, anchor="center", width=150)
+    entry_ky.place(x=1000, y=500, anchor="center", width=150)
 
     entry_xm = Entry(root, width="50")
-    entry_xm.place(x=1000, y=450, anchor="center", width=150)
+    entry_xm.place(x=1000, y=550, anchor="center", width=150)
 
     entry_ym = Entry(root, width="50")
-    entry_ym.place(x=1000, y=500, anchor="center", width=150)
+    entry_ym.place(x=1000, y=600, anchor="center", width=150)
 
     label = Label(root, text="kx:", bg="lavender").place(x=800,
-                                                         y=350, anchor="w", width=100)
-    label = Label(root, text="ky:", bg="lavender").place(x=800,
-                                                         y=400, anchor="w", width=100)
-    label = Label(root, text="Xm:", bg="lavender").place(x=800,
                                                          y=450, anchor="w", width=100)
-    label = Label(root, text="Ym:", bg="lavender").place(x=800,
+    label = Label(root, text="ky:", bg="lavender").place(x=800,
                                                          y=500, anchor="w", width=100)
+    label = Label(root, text="Xm:", bg="lavender").place(x=800,
+                                                         y=550, anchor="w", width=100)
+    label = Label(root, text="Ym:", bg="lavender").place(x=800,
+                                                         y=600, anchor="w", width=100)
+
+    button = Button(text="Вернуть \nначальный рисунок", width=15,
+                    command=return_all, bg="thistle3")
+    button.place(x=1000, y=675, anchor="center")
 
     button_del_all = Button(text="Шаг назад\n(Можно только 1 раз)", width=15,
                             command=cancel, bg="thistle3")
