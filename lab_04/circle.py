d6271c934df76aa0d9ecbcf558345427c132732f
Sukocheva Alice <sukocheva.alis@mail.ru>
2020-04-06 18:01:17 +0300

    Ничего не работает, но мы не расстраиваемся

--- lab_04/circle.py
+++ lab_04/circle.py
@@ -4,19 +4,38 @@ from math import *
 from draw import *
 
 
+def symmetrical_reflection(list_points):
+    # complete the figure.
+    # n = len(list_points)
+    # for i in range(n - 1, 0, -1):
+    #     list_points.append([list_points[i][1], list_points[i][0]])
+
+    # n = len(list_points)
+    # for i in range(n):
+    #     list_points.append([list_points[i][0], HEIGHT - list_points[i][1]])
+
+    # n = len(list_points)
+    # for i in range(n):
+    #     list_points.append([WIDTH - list_points[i][0], list_points[i][1]])
+
+    n = len(list_points)
+
+
 def parametric_circle(center, radius):
-    list_points = list()
+    # list_points = list()
 
-    if radius == 0:
-        list_points.append([center[0], center[1]])
-        return
+    # if radius == 0:
+    #     list_points.append([center[0], center[1]])
+    #     return
 
-    for t in arange(0, -pi/2, -1 / radius):
-        x = radius * cos(t) + center[0]
-        y = radius * sin(t) + center[1]
-        list_points.append([x, y])
+    # step = 1 / radius
+    # for t in arange(0, pi/4 + step, step):
+    #     x = radius * cos(t) + center[0]
+    #     y = radius * sin(t) + center[1]
+    #     list_points.append([x, y])
 
-    return list_points
+    # return list_points
+    pass
 
 
 def draw_circle(canvas_class, method, center, radius):
@@ -42,4 +61,5 @@ def draw_circle(canvas_class, method, center, radius):
         canvas_class.draw_oval(
             center[0] - radius, center[1] - radius, center[0] + radius, center[1] + radius)
 
+    symmetrical_reflection(list_points)
     canvas_class.draw_figure(list_points)
