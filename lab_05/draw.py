ea8357be887c19a660f4b0d4b397425ff50556b3
Sukocheva Alice <sukocheva.alis@mail.ru>
2020-04-12 13:32:15 +0300

    Update

--- lab_05/draw.py
+++ lab_05/draw.py
@@ -4,7 +4,7 @@ from constants import *
 
 from functions_answer import get_two_answer
 from interface import print_error
-from fill import bresenham_int
+from fill import find_intersections
 
 
 class paint_class():
@@ -45,9 +45,13 @@ class paint_class():
             self.img.put(color_bg[1], (round(x), round(y)))
             # print("Реверснуть на цвет фона")
 
+    def print_line(self, start, stop):
+        self.canvas.create_line(
+            start[0], start[1], stop[0], stop[1], fill=self.color_line[1], tags="user_line")
+
     def print_pixel(self, x, y):
         self.canvas.create_line(round(x), round(y), round(
-            x), round(y) + 1, width=1, fill=self.color_line[1], tags="user_line")
+            x), round(y) + 1, width=1, fill=self.color_line[1])  # , tags="user_line")
         # self.img.put(self.color_line[1], (round(x), round(y)))
 
     def choose_color_line(self):
@@ -60,10 +64,11 @@ class paint_class():
 
 def lock(points_list, canvas_class, list_box, lst):
     if len(points_list[-1]) > 1:
-        start = points_list[-1][0]
-        stop = points_list[-1][-1]
+        start = points_list[-1][-1]
+        stop = points_list[-1][0]
 
-        bresenham_int(canvas_class, start, stop, lst)
+        find_intersections(canvas_class, start, stop, lst)
+        canvas_class.print_line(start, stop)
 
         points_list.append(list())
         list_box.insert(END, "_" * 8)
@@ -81,14 +86,16 @@ def add_point(points_list, point, canvas_class, list_box, lst):
     # print(points_list[-1])
 
     if len(points_list[-1]) > 1:
-        start = points_list[-1][-1]
-        stop = points_list[-1][-2]
+        start = points_list[-1][-2]
+        stop = points_list[-1][-1]
 
-        bresenham_int(canvas_class, stop, start, lst)
+        find_intersections(canvas_class, stop, start, lst)
+        canvas_class.print_line(start, stop)
+
     elif len(points_list[-1]) == 1:
         canvas_class.print_pixel(points_list[-1][0][0], points_list[-1][0][1])
-        if [points_list[-1][0][0], points_list[-1][0][1]] not in lst:
-            lst.append([points_list[-1][0][0], points_list[-1][0][1]])
+        # if [points_list[-1][0][0], points_list[-1][0][1]] not in lst:
+        # lst.append([points_list[-1][0][0], points_list[-1][0][1]])
 
 
 def add_point_click(event, canvas_class, list_box, points_list, lst):
--- lab_05/fill.py
+++ lab_05/fill.py
@@ -1,52 +1,22 @@
 from math import fabs
 
 
-def sign(a):
-    if a == 0:
-        return 0
-    return a / abs(a)
+def find_intersections(canvas_class, start, stop, lst):
+    if stop[1] == start[1]:
+        return
 
+    if start[1] > stop[1]:
+        start, stop = stop, start
 
-def bresenham_int(canvas_class, start, stop, lst):
-    dx = stop[0] - start[0]
-    dy = stop[1] - start[1]
-    x, y = start[0], start[1]
-    sx, sy = sign(dx), sign(dy)
-    dx = fabs(dx)
-    dy = fabs(dy)
+    dy = 1
+    dx = (stop[0] - start[0]) / (stop[1] - start[1])
+    x = start[0]
+    y = start[1]
 
-    swap = 0
-
-    if dy > dx:
-        swap = 1
-        dx, dy = dy, dx
-
-    e = 2 * dy - dx
-
-    lst.append([int(x), int(y)])
-
-    for _ in range(int(dx + 1)):
-        canvas_class.print_pixel(int(x), int(y))
-        if e >= 0:
-            if swap == 0:
-                y += sy
-                if start[1] != stop[1]:  # and [int(x), int(y)] not in lst:
-                    lst.append([int(x), int(y)])
-            else:
-                x += sx
-            e -= (2 * dx)
-
-        if e < 0:
-            if swap == 0:
-                x += sx
-            else:
-                y += sy
-                if start[1] != stop[1]:  # and [int(x), int(y)] not in lst:
-                    lst.append([int(x), int(y)])
-            e += (2 * dy)
-    lst.append([int(x), int(y)])
-
-    # print(lst)
+    while y < stop[1]:
+        lst.append([int(x), int(y)])
+        y += dy
+        x += dx
 
 
 def fill(canvas_class, lst):
@@ -62,12 +32,6 @@ def fill(canvas_class, lst):
 
 
 def delayed_fill(canvas_class, lst):
-    # for i in range(len(lst)):
-    #     for j in range(lst[i][0], WIDTH):
-    #         canvas_class.canvas.after(
-    #             1, canvas_class.reverse_pixel(j, lst[i][1]))
-    #         canvas_class.canvas.update()
-
     septum = lst[len(lst) // 2][0]
     print(septum)
     for i in range(len(lst)):
