b33b185eea4921ac71b3ceb5aac2885916d21b3c
Sukocheva Alice <sukocheva.alis@mail.ru>
2020-04-22 13:51:26 +0300

    Отражает по z

--- lab_04/circle.py
+++ lab_04/circle.py
@@ -4,7 +4,7 @@ from math import *
 from draw import *
 
 
-def symmetrical_reflection(list_points):
+def symmetrical_reflection(list_points, center):
     # complete the figure.
     # n = len(list_points)
     # for i in range(n - 1, 0, -1):
@@ -20,22 +20,31 @@ def symmetrical_reflection(list_points):
 
     n = len(list_points)
 
+    for i in range(n):
+        x = list_points[i][0]
+        x -= center[0]
+        y = list_points[i][1]
+        y -= center[1]
+        x, y = y, x
+        x += center[0]
+        y += center[1]
+        list_points.append([x, y])
+
 
 def parametric_circle(center, radius):
-    # list_points = list()
+    list_points = list()
 
-    # if radius == 0:
-    #     list_points.append([center[0], center[1]])
-    #     return
+    if radius == 0:
+        list_points.append([center[0], center[1]])
+        return
 
-    # step = 1 / radius
-    # for t in arange(0, pi/4 + step, step):
-    #     x = radius * cos(t) + center[0]
-    #     y = radius * sin(t) + center[1]
-    #     list_points.append([x, y])
+    step = 1 / radius
+    for t in arange(0, pi/4 + step, step):
+        x = radius * cos(t) + center[0]
+        y = radius * sin(t) + center[1]
+        list_points.append([x, y])
 
-    # return list_points
-    pass
+    return list_points
 
 
 def draw_circle(canvas_class, method, center, radius):
@@ -61,5 +70,5 @@ def draw_circle(canvas_class, method, center, radius):
         canvas_class.draw_oval(
             center[0] - radius, center[1] - radius, center[0] + radius, center[1] + radius)
 
-    symmetrical_reflection(list_points)
+    symmetrical_reflection(list_points, center)
     canvas_class.draw_figure(list_points)
