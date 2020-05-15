409841893b76e01dc656fca27fdbe8dc46e4ff4f
Sukocheva Alice <sukocheva.alis@mail.ru>
2020-05-15 19:25:17 +0300

    Материалы и основа для лр 9

---
+++ lab_09/constants.py
@@ -0,0 +1,21 @@
+# FOR INTERFACE #
+
+WIDTH, HEIGHT = 800, 800
+
+FALSE = "-"
+
+COLOR_BG = ((255, 255, 255), 'white')
+COLOR_FILL_INIT = ((0.0, 0.0, 0.0), "#000000")
+COLOR_LINE = ((136, 0, 250), "#8800fa")
+
+FONT = "Verdana 12"
+
+# FOR LINE #
+
+X = 0
+Y = 1
+
+X1 = 0
+Y1 = 1
+X2 = 2
+Y2 = 3
